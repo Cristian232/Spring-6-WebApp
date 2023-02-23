c2e0770d35ea393a50a13780866b6ee97a88e097
Cristian <Cristi@noemail.com>
2023-02-23 05:16:57 +0200

    Added Author Service and impl also added AuthorController // SPR9-SPR10 S29

---
+++ src/main/java/com/spring6webapp/service/AuthorService.java
@@ -0,0 +1,9 @@
+package com.spring6webapp.service;
+
+import com.spring6webapp.domain.Author;
+
+public interface AuthorService {
+
+    Iterable<Author> findAll();
+
+}
---
+++ src/main/java/com/spring6webapp/service/AuthorServiceImpl.java
@@ -0,0 +1,22 @@
+package com.spring6webapp.service;
+
+import com.spring6webapp.domain.Author;
+import com.spring6webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorServiceImpl implements AuthorService {
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorServiceImpl(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @Override
+    public Iterable<Author> findAll() {
+
+       return authorRepository.findAll();
+
+    }
+}
